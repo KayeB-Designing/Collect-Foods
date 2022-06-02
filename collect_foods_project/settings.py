a334d5f2dc42f0b951d1cb7b6c04d38ae77c95db
Kaye Bryant <bryantk72391@gmail.com>
2022-06-02 19:51:00 -0400

    troubleshooting deployment

--- collect_foods_project/settings.py
+++ collect_foods_project/settings.py
@@ -4,7 +4,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# STATIC_ROOT = BASE_DIR / 'main_app/static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 # Default primary key field type
